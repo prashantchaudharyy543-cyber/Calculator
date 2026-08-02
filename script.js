0ed61dd7a950f35b5efec72e34601f412b8593ab
Prashant chaudhary <prashantchaudharyy543@gmail.com>
2026-08-02 20:20:48 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const display=document.getElementById("display");
+
+function append(value){
+display.value+=value;
+}
+
+function clearDisplay(){
+display.value="";
+}
+
+function deleteLast(){
+display.value=display.value.slice(0,-1);
+}
+
+function calculate(){
+
+try{
+
+let expression=display.value.replace(/%/g,"/100");
+
+display.value=eval(expression);
+
+}
+
+catch{
+
+display.value="Error";
+
+}
+
+}
